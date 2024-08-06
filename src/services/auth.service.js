567a5bbda9f4a49f754004af8d1a42c40f4d99c6
Đỗ Quang Khải <88335848+QuangKhai-dev@users.noreply.github.com>
2024-08-06 21:09:23 +0700

    update

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,7 +1,10 @@
-import { http } from "./config"
+import { http } from "./config";
 
 export const authService = {
   signUp: (data) => {
-    return http.post("/auth/signup",data)
-  }
-}
+    return http.post("/auth/signup", data);
+  },
+  signIn: (data) => {
+    return http.post("/auth/signin", data);
+  },
+};
