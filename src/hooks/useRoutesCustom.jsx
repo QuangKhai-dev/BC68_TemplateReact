a7151efee8a97794f09262628d6d79adaf22d974
Đỗ Quang Khải <88335848+QuangKhai-dev@users.noreply.github.com>
2024-08-14 09:34:44 +0700

    admin template - xác thực - redux thunk - render table người dùng

--- src/hooks/useRoutesCustom.jsx
+++ src/hooks/useRoutesCustom.jsx
@@ -4,12 +4,22 @@ import UserTemplate from "../templates/UserTemplate/UserTemplate";
 import { pathDefault } from "../common/path";
 import RegisterPage from "../pages/Register/RegisterPage";
 import LoginPage from "../pages/Login/LoginPage";
+import ListJobPage from "../pages/ListJobPage/ListJobPage";
+import AdminTemplate from "../templates/AdminTemplate/AdminTemplate";
+import AdminLogin from "../pages/AdminLogin/AdminLogin";
+import ManagerUser from "../pages/ManagerUser/ManagerUser";
 
 const useRoutesCustom = () => {
   const routes = useRoutes([
     {
       path: pathDefault.homePage,
       element: <UserTemplate />,
+      children: [
+        {
+          path: pathDefault.listJob,
+          element: <ListJobPage />
+        }
+      ]
     },
     {
       path: pathDefault.register,
@@ -19,6 +29,25 @@ const useRoutesCustom = () => {
       path: pathDefault.login,
       element: <LoginPage />,
     },
+    {
+      path: pathDefault.admin,
+      element: <AdminTemplate />,
+      children: [
+        {
+          // path: "manager-user",
+          index: true,
+          element: <ManagerUser />
+        },
+        {
+          path: "manager-user",
+          // index: true,
+          element: <ManagerUser />
+        },
+      ]
+    }, {
+      path: '/admin-login',
+      element: <AdminLogin />
+    }
   ]);
   return routes;
 };
---
+++ src/services/nguoiDung.service.js
@@ -0,0 +1,7 @@
+import { http } from "./config";
+
+export const nguoiDungService = {
+  getAllUsers: () => {
+    return http.get("/users");
+  },
+};
